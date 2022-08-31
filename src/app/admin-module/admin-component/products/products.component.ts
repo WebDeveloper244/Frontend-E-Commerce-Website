a0f5f5618d28db18c9600003db0fb094d08bab42
WebDevolper1122 <sb2137842@gmail.com>
2022-09-01 00:05:37 +0500

    commit

--- src/app/admin-module/admin-component/products/products.component.ts
+++ src/app/admin-module/admin-component/products/products.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProductsComponent implements OnInit {
  color:any=["Red","Black","Blue"];
  size:any=["Small","Medium","Large","X-Large","XX-Large"];
+ categories=["Cap","Hoodies","Watch","Bags"]
   constructor() { }
 
   ngOnInit(): void {
