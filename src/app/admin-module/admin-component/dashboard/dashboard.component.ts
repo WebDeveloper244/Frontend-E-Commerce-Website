c233af07ea74fa2a115bf8e7c32aeecb634c295b
WebDevolper1122 <sb2137842@gmail.com>
2022-09-02 00:46:23 +0500

    commit

--- src/app/admin-module/admin-component/dashboard/dashboard.component.ts
+++ src/app/admin-module/admin-component/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JsonDataService } from 'src/app/shared-portal/JsonService/json-data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+dataComeFromJson:any=[]
+  constructor(
+    private jsonData:JsonDataService
+  ) { 
+    this.jsonData1()
+  }
 
   ngOnInit(): void {
+  }
+  jsonData1(){
+    this.dataComeFromJson = this.jsonData.getjsonData()
   }
 
 }
--- src/app/admin-module/admin-component/products/products.component.ts
+++ src/app/admin-module/admin-component/products/products.component.ts
@@ -11,9 +11,9 @@ export class ProductsComponent implements OnInit {
  color=["Red","Black","Blue"];
  categories=["Cap","Hoodies","Watch","Bags"];
  selectSize=["S","M","L","X-L","XX-L"];
- newSizeArray:any=[]
- 
- 
+ newSizeArray:any=[];
+ imageArray:any=[];
+ disableButtonTrue: boolean=false;
 
  myProductForm:FormGroup|any
   constructor(
@@ -25,42 +25,49 @@ export class ProductsComponent implements OnInit {
 
   buildForm(){
     this.myProductForm = this.formBuilder.group({
-     productName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
-      quantity: ['', [Validators.required, Validators.pattern(/^[0-9]*$/)]],
-      price: ['', [Validators.required, Validators.pattern(/^[0-9]*$/)]],
-      description: ['', Validators.required],
-      color: ['',[Validators.required,Validators.pattern(/^[A-Za-z]+$/)]],
-      companyName: ['',[Validators.required,Validators.pattern(/^[A-Za-z]+$/)]],
-      // logoMaterial: ['',[Validators.required,Validators.pattern(/^[A-Za-z]+$/)]],
-      category: ['', Validators.required],
+     productName: new FormControl ('', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]),
+      quantity: new FormControl ('', [Validators.required, Validators.pattern(/^[0-9]*$/)]),
+      price: new FormControl ('', [Validators.required, Validators.pattern(/^[0-9]*$/)]),
+      description: new FormControl ('', Validators.required),
+      color: new FormControl ('',[Validators.required,Validators.pattern(/^[A-Za-z]+$/)]),
+      companyName: new FormControl ('',[Validators.required,Validators.pattern(/^[A-Za-z]+$/)]),
+      category:new FormControl ('', Validators.required),
       size: new FormArray([]),
-      productMaterial:['',Validators.required],
+      productMaterial:new FormControl('', Validators.required),
       image: new FormArray([])
     })
   }
+
   getSize(event:any){
     if(event.target.checked){
      this.newSizeArray.push(event.target.value)
     }
     else{
-     this.newSizeArray = this.newSizeArray.filter((value:any)=>value != event.target.value)
+     this.newSizeArray = this.newSizeArray.filter((value:any)=>value != event.target.value) 
     }
   }
+
+  getImages(event:any){
+  if(event.target.files.length <= 5){
+this.imageArray.push(event.target.files)
+  }else{
+    this.imageArray = [];
+    this.disableButtonTrue = true
+  }
+  }
   
-
-
  submitProductForm(){
-  // console.log(this.ProductForm.get('companyName').setValue('Rameen is the new hacker of the class')); Gettter and Sstter (it get and set the value any Object(productForm))
   this.newSizeArray.forEach((elements:string)=>{
    let formControl = new FormControl(elements)
    this.myProductForm.get("size").push(formControl)
   })
+  this.imageArray.forEach((element:any) => {
+   let formControl = new FormControl(element)
+   this.myProductForm.get("image").push(formControl) 
+  })
  let result= this.myProductForm.value;
   console.log(result);
-  
-
-
-  
   }
 
 }
+// console.log(this.ProductForm.get('companyName').setValue('Rameen is the new hacker of the class')); Gettter and Sstter (it get and set the value any Object(productForm))
