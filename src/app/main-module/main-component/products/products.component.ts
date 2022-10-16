9b4b33bc960f02bd75fb0573d82c2e36ab6c11dd
WebDevolper1122 <sb2137842@gmail.com>
2022-10-16 23:30:23 +0500

    commit

--- src/app/main-module/main-component/products/products.component.ts
+++ src/app/main-module/main-component/products/products.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { JsonDataService } from 'src/app/shared-portal/JsonService/json-data.service';
+import { MessangerService } from 'src/app/shared-portal/rxjs-messanger/messanger.service';
+import { SavingDataServiceService } from 'src/app/shared-portal/volatile-service/saving-data-service.service';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private JsonService:JsonDataService,
+    private Router : Router,
+    private messanger : MessangerService,
+    private volatileService :SavingDataServiceService
+  ) { }
+
+  DataComeFromJson :any=[];
 
   ngOnInit(): void {
+  }
+  getDataFromservice(){
+    this.DataComeFromJson = this.JsonService.getjsonData();
+  }
+
+  goToCartPage(Id:any){
+   
+ 
+    this.volatileService.saveDataToLocalStorage(Id)
+    this.messanger.sendMessageFromMyData(Id)
+    this.Router.navigate(['view-product']);
+ 
   }
 
 }
